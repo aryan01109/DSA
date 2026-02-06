b1d4fbed02e84f68a746dfb975cd477c56baef0d
ARYAN BHOYA <aryanbhoya01@gmail.com>
2026-02-06 08:22:40 +0530

    new file add

---
+++ Stack/implemation.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Stack{
+    vector<int>vec;
+
+    public :
+            void push(){
+
+            }
+
+            void print(){
+
+            }
+            void pop(){
+
+            }
+
+            void peek(){
+
+            }
+
+            void del(){
+
+                
+            }
+
+
+};
+
+int main(){
+    Stack s;
+
+    return 0;
+}
