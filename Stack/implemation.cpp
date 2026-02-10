a719ab6a95c75a53b20d71088f4cb866ce11ad04
ARYAN BHOYA <aryanbhoya01@gmail.com>
2026-02-10 09:06:56 +0530

    file update

--- Stack/implemation.cpp
+++ Stack/implemation.cpp
@@ -1,36 +1,53 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
 
-class Stack{
-    vector<int>vec;
+template <typename T>
+class Stack {
+    vector<T> vec;
 
-    public :
-            void push(){
+public:
+    void push(T val) {
+        vec.push_back(val);
+    }
 
-            }
+    void pop() {
+        if (isEmpty()) {
+            cout << "Stack is empty" << endl;
+            return;
+        }
+        vec.pop_back();
+    }
 
-            void print(){
+    void top() {
+        if (isEmpty()) {
+            cout << "Stack is empty" << endl;
+            return;
+        }
+        cout << vec.back() << endl;
+    }
 
-            }
-            void pop(){
-
-            }
-
-            void peek(){
-
-            }
-
-            void del(){
-
-                
-            }
-
-
+    bool isEmpty() {
+        return vec.empty();
+    }
 };
 
-int main(){
-    Stack s;
+int main() {
+    Stack<char> s;
+
+    s.push('a');
+    s.push('b');
+    s.push('c');
+
+    s.top();   // c
+    s.pop();
+    s.top();   // b
+
+    cout << "\nStack elements are:\n";
+    while (!s.isEmpty()) {
+        s.top();
+        s.pop();
+    }
 
     return 0;
 }
